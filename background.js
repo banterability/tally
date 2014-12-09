835b3fdd0697e04a8f95c9f4714fd0a01f3dab41
Jeffrey Long <jlong@groupon.com>
2014-12-09 14:33:10 -0600

    Flash on tab count change

--- background.js
+++ background.js
@@ -1,17 +1,34 @@
-function updateCount(tabCount){
+function setCount(tabCount){
+  chrome.browserAction.setBadgeBackgroundColor({color: [0, 0, 255, 255]});
   chrome.browserAction.setBadgeText({text: tabCount.toString()});
 }
 
-function countTabs(){
+function setDirection(direction){
+  if(direction === '+'){
+    chrome.browserAction.setBadgeBackgroundColor({color: [0, 255, 0, 255]});
+  } else {
+    chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 255]});
+  }
+  chrome.browserAction.setBadgeText({text: direction});
+}
+
+function update(direction){
   chrome.tabs.query({}, function(tabs){
-    updateCount(tabs.length);
+    if(direction){
+      setDirection(direction);
+      setTimeout(function(){
+        setCount(tabs.length);
+      }, 500);
+    } else {
+      setCount(tabs.length);
+    }
   });
 }
 
 function init(){
-  chrome.tabs.onCreated.addListener(countTabs);
-  chrome.tabs.onRemoved.addListener(countTabs);
-  countTabs();
+  chrome.tabs.onCreated.addListener(function(){update('+');});
+  chrome.tabs.onRemoved.addListener(function(){update('-');});
+  update();
 }
 
 init();
